d7a09e592dfbe970554a7035223de14b7130ddad
DanGitUser <imtaehoonkim@gmail.com>
2026-02-28 23:13:27 +0900

    new Spring Project overhauling my last

---
+++ src/main/java/com/hub/allroadshub/AllRoadsHubApplication.java
@@ -0,0 +1,13 @@
+package com.hub.allroadshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllRoadsHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllRoadsHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hub/allroadshub/AllRoadsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hub.allroadshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllRoadsHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
